61c7dd549401c369455ce231232ff7340303b62f
Tomasz Pleśniak <toma1777@easv365.dk>
2018-12-04 17:35:00 +0100

    small fixes

--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
-import {Login} from '../models/';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +9,16 @@ import {Login} from '../models/';
 export class AuthenticationService {
 
   constructor(private http: HttpClient) { }
-  apiUrl = 'https://mexicanfooddeveloper.azurewebsites.net/api/token';
+  apiUrl = 'https://mexicanfooddeveloper.azurewebsites.net/api/tokens';
 
-  login(login: Login): Observable<boolean> {
-    return this.http.post<any>(this.apiUrl, { login })
+  login(username: string, password: string): Observable<boolean> {
+    return this.http.post<any>(this.apiUrl, { username, password  })
       .pipe(map(response => {
         const token = response && response.token;
         // login successful if there's a jwt token in the response
         if (token) {
           // store username and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify({username: login.username, token: token}));
+          localStorage.setItem('currentUser', JSON.stringify({username: username, token: token}));
           // return true to indicate successful login
           return true;
         } else {
