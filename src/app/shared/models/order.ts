6ea7bb8d75342896a1eaed779531a7b8107aab83
Anni <32060255+eneaneli@users.noreply.github.com>
2018-12-12 13:55:35 +0100

    refactored dates and orderlist

--- src/app/shared/models/order.ts
+++ src/app/shared/models/order.ts
@@ -4,8 +4,8 @@ export class Order {
   id?: number;
   mobilenumber: string;
   customerName: string;
-  pickUpDate?: Date;
-  orderDate?: Date;
+  pickUpDateAndTime?: Date;
+  orderedDateAndTime?: Date;
   comment: string;
   orderLines?: OrderLine[];
 
