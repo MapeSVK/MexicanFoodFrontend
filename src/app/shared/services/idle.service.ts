bff433d35e9dab345fe557fed115ba4be7a2b903
Tomasz Pleśniak <toma1777@easv365.dk>
2018-12-19 10:31:23 +0100

    remove alive

--- src/app/shared/services/idle.service.ts
+++ src/app/shared/services/idle.service.ts
@@ -12,18 +12,18 @@ import {DialogWindowComponent} from '../dialog-window/dialog-window.component';
 export class IdleService {
   idleState = 'Not started.';
 
-  constructor(private idle: Idle, private keepalive: Keepalive, private authenticationService: AuthenticationService, public dialog: MatDialog) {
+  constructor(private idle: Idle, private authenticationService: AuthenticationService, public dialog: MatDialog) {
     this.idle.setIdle(175);
     this.idle.setTimeout(5);
     this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
     this.idle.onIdleEnd.subscribe(() => this.idleState = 'No longer idle.');
     this.idle.onTimeout.subscribe(() => {
+      //Time out
       this.idleState = 'Timed out!';
       this.dialog.open(DialogWindowComponent);
       this.logOut();
     });
     this.idle.onIdleStart.subscribe(() => this.idleState = 'You\'ve gone idle!');
-    this.keepalive.interval(15);
   }
 
   logOut() {
